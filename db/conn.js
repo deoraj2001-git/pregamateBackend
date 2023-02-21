34a781ca50630e2a48b07a41f5fdec6b06272a7c
Deoraj Maharaj <101578606+deoraj2001-git@users.noreply.github.com>
2023-02-21 18:41:19 +0530

    Update conn.js

---
+++ db/conn.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const username = 'care@pregamate.com';
+const password = '27May@2011';
+const clusterName = 'cluster0';
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@${clusterName}.mongodb.net/<your-db-name>?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB!');
+});
